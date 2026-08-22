fc9ff22928152fead6dd35aec3154befcfd91fa7
Kota Karthik <24311a04eg@ece.sreenidhi.edu.in>
2026-08-22 22:44:44 +0530

    Time: 0 ms (100%), Space: 9 MB (37.68%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for(int i=0;i<s.length();i++){
+            if(!stk.empty() && ((stk.top() == '(' && s[i] == ')') || (stk.top() == '[' && s[i] == ']') || (stk.top() == '{' && s[i] == '}'))){
+                stk.pop();
+            }else{
+                stk.push(s[i]);
+            }
+        }
+        return stk.empty();
+    }
+};
