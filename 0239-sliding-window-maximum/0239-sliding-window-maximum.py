4f8a6776d1f6490ffb16a59abefd2d2aeda5920a
Kota Karthik <24311a04eg@ece.sreenidhi.edu.in>
2026-09-23 21:41:36 +0530

    Time: 215 ms (23.27%), Space: 35.2 MB (48.01%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        n = len(nums)
+        if n * k == 0:
+            return []
+        if k == 1:
+            return nums
+
+        dq = deque()
+        result = []
+
+        for i in range(n):
+            while dq and dq[0] < i - k + 1:
+                dq.popleft()
+            while dq and nums[dq[-1]] < nums[i]:
+                dq.pop()
+            dq.append(i)
+            if i >= k - 1:
+                result.append(nums[dq[0]])
+
+        return result
